7986af51c85ac82b83531b484fcf2b4e8b49efdc
SvenFE <shensven2016@gmail.com>
2023-01-29 20:28:32 +0800

    first commit

---
+++ Morphling/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Morphling
+//
+//  Created by SvenFE on 2023/1/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Morphling/MorphlingApp.swift
@@ -0,0 +1,17 @@
+//
+//  MorphlingApp.swift
+//  Morphling
+//
+//  Created by SvenFE on 2023/1/29.
+//
+
+import SwiftUI
+
+@main
+struct MorphlingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
